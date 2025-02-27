3c3ee84c1db048fba95ac97a1ef5b1aa56da004b
naumaan12 <zafar121malik@gmail.com>
2025-02-27 13:31:01 +0530

    dashboard api update

--- src/handlers/dashboard.ts
+++ src/handlers/dashboard.ts
@@ -2,6 +2,7 @@ import { Context } from "hono";
 import TaskModel from "../models/taskModel";
 import UserModel from '../models/userModel';
 import PackageModel from '../models/packageModel'; 
+import InvestmentModel from '../models/investmentModel'; 
 
 
 // Get All Tasks
@@ -11,8 +12,14 @@ export const dashboard = async (c: Context) => {
         const blockedUserCount = await UserModel.countDocuments({ role: "USER", status: "BLOCK" });
         const activePackageCount = await PackageModel.countDocuments({ status: "ACTIVE" });
         const activeTaskCount = await TaskModel.countDocuments({ status: "ACTIVE" });
+        const totalBuyInvestmentPlans = await InvestmentModel.aggregate([
+            { $unwind: "$buyPackagesDetails" }, // Unwind the array to count each package separately
+            { $count: "total" } // Count the total number of bought investment plans
+        ]);
+        
+        const totalSubscriptionCount = totalBuyInvestmentPlans.length > 0 ? totalBuyInvestmentPlans[0].total : 0;
 
-        return c.json({ message: "dashboard data fetch successfully", userCount: userCount ,blockUser: blockedUserCount, activePackageCount: activePackageCount, activeTaskCount:activeTaskCount, platefromTotalEarnig: 0 }, 200);
+        return c.json({ message: "dashboard data fetch successfully", userCount: userCount ,blockUser: blockedUserCount, activePackageCount: activePackageCount, activeTaskCount:activeTaskCount, platefromTotalEarnig: 0, totalSubscriptionCount:totalSubscriptionCount }, 200);
 
     } catch (error) {
         console.error('Error fetching tasks:', error);
