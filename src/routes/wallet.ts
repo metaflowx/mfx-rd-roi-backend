8208da37fff5ea62bd6c5751b3e41908f1bce7bb
naumaan12 <zafar121malik@gmail.com>
2025-03-05 12:47:17 +0530

    wallet routing fix

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -1,11 +1,11 @@
 import { Hono } from 'hono';
 import { wallet } from '../handlers';
-import { protect } from '../middleware';
+import { protect, isAdmin } from '../middleware';
 
 const walletRoutes = new Hono();
 
 walletRoutes.get('/detail',protect,(c)=> wallet.userWallet(c))
-walletRoutes.put('/update',protect,(c)=> wallet.updateWalletBalanceByAdmin(c))
+walletRoutes.put('/update',protect, isAdmin, (c)=> wallet.updateWalletBalanceByAdmin(c))
 walletRoutes.get('/asset/balance',protect, (c) => wallet.userBalanceAtAsset(c)); 
 walletRoutes.get('/asset/balances',protect, (c) => wallet.totalUserBalanceAtAsset(c)); 
 
