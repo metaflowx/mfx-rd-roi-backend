dbd5df9114328772240613528a621a24237321bc
naumaan12 <zafar121malik@gmail.com>
2025-03-05 12:38:23 +0530

    wallet routing fix

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -4,9 +4,9 @@ import { protect } from '../middleware';
 
 const walletRoutes = new Hono();
 
-walletRoutes.get('detail',protect,(c)=> wallet.userWallet(c))
+walletRoutes.get('/detail',protect,(c)=> wallet.userWallet(c))
 walletRoutes.put('/update',protect,(c)=> wallet.updateWalletBalanceByAdmin(c))
-walletRoutes.get('asset//balance',protect, (c) => wallet.userBalanceAtAsset(c)); 
-walletRoutes.get('asset/balances',protect, (c) => wallet.totalUserBalanceAtAsset(c)); 
+walletRoutes.get('/asset/balance',protect, (c) => wallet.userBalanceAtAsset(c)); 
+walletRoutes.get('/asset/balances',protect, (c) => wallet.totalUserBalanceAtAsset(c)); 
 
 export default walletRoutes;
